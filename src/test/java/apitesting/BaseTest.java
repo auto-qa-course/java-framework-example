41676c70d2ffb4ef85ab8f55f8be65ce3a41ff96
Inna Ianko <inna.ianko@am.jll.com>
2018-11-27 18:37:27 +0200

    TestNG setup.

--- src/test/java/apitesting/BaseTest.java
+++ src/test/java/apitesting/BaseTest.java
@@ -1,17 +1,15 @@
 package apitesting;
 
-import org.testng.annotations.Test;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
     @BeforeSuite()
-    public void deleteAllureOutputsFolder() {
+    public void testSetup() {
         System.out.println("Setup test preconditions.");
     }
 
     public BaseTest() {
         System.out.println("Let's write some test cases!");
     }
-
 }
