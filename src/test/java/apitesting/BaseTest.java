b18cb2f33c3ee99e4c48f59e353430120a657eb2
Inna Ianko <inna.ianko@am.jll.com>
2018-11-27 18:56:00 +0200

    Setup RestAssured with example.

--- src/test/java/apitesting/BaseTest.java
+++ src/test/java/apitesting/BaseTest.java
@@ -1,6 +1,11 @@
 package apitesting;
 
 import org.testng.annotations.BeforeSuite;
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class BaseTest {
 
@@ -11,5 +16,17 @@ public class BaseTest {
 
     public BaseTest() {
         System.out.println("Let's write some test cases!");
+
+        RestAssured.baseURI = "http://httpbin.org/";
+
+        get("/get").then().assertThat().statusCode(200);
+
+        when().
+                get("/get").
+        then().
+                assertThat().statusCode(200).
+                assertThat().body("url", equalTo("http://httpbin.org/get"));
+
     }
+
 }
