be1ee120c6e60ca3b0877a8647c750bb3b4342bf
Inna Ianko <inna.ianko@am.jll.com>
2018-11-29 14:53:21 +0200

    Added sample tests for httpbin.org

--- src/test/java/apitesting/BaseTestSuite.java
+++ src/test/java/apitesting/BaseTestSuite.java
@@ -1,6 +1,7 @@
 package apitesting;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
 import io.restassured.RestAssured;
 import io.qameta.allure.*;
 
@@ -11,15 +12,28 @@ import static org.hamcrest.Matchers.*;
 @Story("Test Story")
 public class BaseTestSuite {
 
-    @Test(description = "Test GET for httpbin.org")
-    @Description("Some detailed test description")
-    public void BaseTest() {
+    @BeforeClass
+    public void SetupBaseURL() {
         RestAssured.baseURI = "http://httpbin.org/";
+    }
 
+    @Test(description = "Test /get endpoint.")
+    public void TestRootGet() {
         when().
                 get("/get").
         then().
                 assertThat().statusCode(200).
                 assertThat().body("url", equalTo("http://httpbin.org/get"));
+    }
+
+    @Test(description = "Test /cookies endpoint.")
+    public void TestCookies() {
+
+        String response = get("/cookies").asString();
+        System.out.println(response);
+
+        response = given().header("accept", "text/plain")
+                .get("/cookies/set/{key}/{value}", "new_key", "new_value").asString();
+        System.out.println(response);
     }
 }
