bf0bf29c976cde2ff9b9bd4a4e3baf6060cd17c7
Inna Ianko <inna.ianko@am.jll.com>
2018-12-07 17:05:49 +0200

    Extended example test cases for GET /users endpoint.

--- src/test/java/apitesting/UsersServiceTest.java
+++ src/test/java/apitesting/UsersServiceTest.java
@@ -25,9 +25,9 @@ public class UsersServiceTest {
                 assertThat().statusCode(200);
     }
 
-    @Test(description = "Test GET Users pagination - page parameter.")
+    @Test(description = "Test GET Users pagination - page number parameter.")
     public void TestUsersGetPaginationByPageNumber() {
-        Integer page_number = 1;
+        int page_number = 1;
         when().
                 get(String.format("/users?page=%s", page_number)).
         then().
@@ -35,9 +35,9 @@ public class UsersServiceTest {
                 assertThat().body("page", equalTo(page_number));
     }
 
-    @Test(description = "Test GET Users pagination.")
+    @Test(description = "Test GET Users pagination - page size parameter.")
     public void TestUsersGetPaginationPerPageSize() {
-        Integer page_size = 10;
+        int page_size = 10;
         ArrayList<String> response =
                 when().
                 get(String.format("/users?per_page=%s", page_size)).
@@ -46,5 +46,42 @@ public class UsersServiceTest {
         extract().
                   path("data");
         System.out.println(response);
+    }
+
+    @Test(description = "Test GET Users pagination - page number & size parameter.")
+    public void TestUsersGetPaginationPerPageSizeAndPageNumber() {
+        int page_number = 2;
+        int page_size = 10;
+        ArrayList<String> response =
+                when().
+                        get(String.format("/users?page=%s&per_page=%s", page_number, page_size)).
+                        then().
+                        assertThat().statusCode(200).
+                        extract().
+                        path("data");
+        System.out.println(response);
+    }
+
+    @Test(description = "Test GET Users pagination - empty page.")
+    public void TestUsersGetPaginationPageEmpty() {
+        int page_number = 1;
+        int pages_number =
+        when().
+                get(String.format("/users?page=%s", page_number)).
+                then().
+                assertThat().statusCode(200).
+                extract().
+                path("total_pages");
+        System.out.println(pages_number);
+
+        page_number = pages_number + 1;
+        ArrayList data_content = new ArrayList<>();
+        when().
+                get(String.format("/users?page=%s", page_number)).
+                then().
+                assertThat().statusCode(200).
+                assertThat().body("total_pages", equalTo(pages_number)).
+                assertThat().body("data", equalTo(data_content));
+
     }
 }
