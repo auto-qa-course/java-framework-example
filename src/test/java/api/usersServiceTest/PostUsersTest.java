0a18d085351047e8f2a1625f7a69e2a1b71a6872
inna-ianko <inna.yanko@gmail.com>
2019-01-22 22:48:53 +0200

    Fixed post test.

--- src/test/java/api/usersServiceTest/PostUsersTest.java
+++ src/test/java/api/usersServiceTest/PostUsersTest.java
@@ -1,11 +1,7 @@
 package api.usersServiceTest;
 
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
 
 
 public class PostUsersTest extends UsersBaseTest {
@@ -15,12 +11,14 @@ public class PostUsersTest extends UsersBaseTest {
     @Test(description = "Test root POST users endpoint.")
     public void TestUsersPost() {
         given().
-                formParam("name", "morpheus").
-                formParam("job", "leader").
+                header("content-type", "application/json").
+                body("{\"name\":\"morpheus\",\"job\":\"leader\"}").
         when().
                 post(usersAPI.path).
         then().
-                assertThat().statusCode(201);
+                assertThat().statusCode(201).
+        extract().
+                path("data");
     }
 
 }
